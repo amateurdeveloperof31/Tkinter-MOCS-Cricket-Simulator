2f6cdb73deb61964817ad5c13da81a7033f7ab05
amateurdeveloperof31 <amateurdeveloperof31@gmail.com>
2025-03-06 21:46:00 +0530

    Update index.py

--- index.py
+++ index.py
@@ -10,12 +10,17 @@ class BookCricket:
         self.windows.resizable(False, False)
         self.windows.config(bg='white')
 
-        self.score = 0
-        self.wicket = 0
+        self.total_score = 0
+        self.total_wicket = 0
+        self.total_balls = 0
 
         self.scoreboard_label = Label(self.windows, text="0-0", width=5, height=1, font=("arial", 50, "bold"),
                                       justify=CENTER)
-        self.scoreboard_label.place(relx=0.5, rely=0.2, anchor=CENTER)
+        self.scoreboard_label.place(relx=0.5, rely=0.15, anchor=CENTER)
+
+        self.overs_label = Label(self.windows, text="0.0", width=5, height=1, font=("arial", 12, "bold"),
+                                      justify=CENTER)
+        self.overs_label.place(relx=0.5, rely=0.25, anchor=CENTER)
 
         self.score_label = Label(self.windows, width=5, height=1, font=("arial", 50, "bold"), justify=CENTER)
         self.score_label.place(relx=0.5, rely=0.35, anchor=CENTER)
@@ -29,14 +34,17 @@ class BookCricket:
         self.windows.mainloop()
 
     def scorer(self):
+        self.total_balls += 1
+        overs = f"{int(self.total_balls/6)}.{(self.total_balls % 6)}"
         ball_score = random.randint(0,6)
         if ball_score == 5:
-            self.wicket += 1
+            self.total_wicket += 1
         else:
-            self.score += ball_score
+            self.total_score += ball_score
 
         self.score_label.config(text=ball_score)
-        self.scoreboard_label.config(text=f"{self.score}-{self.wicket}")
+        self.scoreboard_label.config(text=f"{self.total_score}-{self.total_wicket}")
+        self.overs_label.config(text=f"{overs}")
 
         self.score_timeline.config(state=NORMAL)
         self.score_timeline.insert(END, f"{ball_score} ")
