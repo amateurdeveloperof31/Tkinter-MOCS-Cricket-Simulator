917520041142dcd62159601290703acb73be8f24
amateurdeveloperof31 <amateurdeveloperof31@gmail.com>
2025-04-15 22:01:55 +0530

    Update book_cricket.py

--- book_cricket.py
+++ book_cricket.py
@@ -1,8 +1,6 @@
 # ----------------------------------------------------- Imports --------------------------------------------------------
 from tkinter import *
-from tkinter import messagebox, ttk
 from scoreboard import Scoreboarde
-from PIL import Image, ImageTk
 from image_resizer import ImageResizer
 # ------------------------------------------------ Global Variables ----------------------------------------------------
 width = 800
@@ -10,6 +8,7 @@ height = 600
 
 main_bg = "white"
 gray_out_bg = "#cdcdcd"
+charcoal_bg = "#36454F"
 # --------------------------------------------------- Main Class -------------------------------------------------------
 class BookCricket:
     def __init__(self):
@@ -18,25 +17,19 @@ class BookCricket:
         self.windows.title("MOCS")
         self.windows.minsize(width,height)
         self.windows.resizable(False, False)
-        self.windows.config(bg=main_bg)
+        self.windows.config(bg=charcoal_bg)
 
-        # Variables
-        self.teamA = {}
-        self.teamB = {}
-        self.match_overs = None
-
-        self.main_image = ImageResizer("assets/images/main.jpg", 500)
-        self.main_image_label = Label(self.windows, image=self.main_image.image, font=("arial", 40, "bold"),
-                                      justify=CENTER, bg=main_bg, fg="white")
+        self.main_image = ImageResizer("assets/images/main.png", 500)
+        self.main_image_label = Label(self.windows, image=self.main_image.image, bg=charcoal_bg)
         self.main_image_label.place(relx=0, rely=0.5, anchor=W)
 
         self.main_label = Label(self.windows, text="My Own\nCricket\nSimulator", font=("arial", 40, "bold"), justify=CENTER,
-                                    bg=main_bg, fg="green")
+                                    bg=charcoal_bg, fg="#35b690")
         self.main_label.place(relx=0.8, rely=0.4, anchor=CENTER)
 
         self.start_button_image = ImageResizer("assets/images/start.png", 200)
-        self.start_button = Button(self.windows, image=self.start_button_image.image, bg='white',
-                                   activebackground="white", command=self.open_details_form, borderwidth=0)
+        self.start_button = Button(self.windows, image=self.start_button_image.image, bg=charcoal_bg,
+                                   activebackground=charcoal_bg, command=self.open_details_form, borderwidth=0)
         self.start_button.place(relx=0.8, rely=0.75, anchor=CENTER)
 
         self.windows.mainloop()
